5b983d3ab8d3087201956bd882db7de7827f978a
javierjcf <javierjcf@gmail.com>
2018-11-14 18:56:37 +0100

    [FIX]account_payment_custom: No buscar los abonos por rango de fechas

--- project_addons/account_payment_custom/wizard/account_paymnet_line_create.py
+++ project_addons/account_payment_custom/wizard/account_paymnet_line_create.py
@@ -1,6 +1,7 @@
 # © 2018 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields, api, _
+from odoo import models, fields, api
+from odoo.osv import expression
 
 
 class AccountPaymentLineCreate(models.TransientModel):
@@ -13,8 +14,41 @@ class AccountPaymentLineCreate(models.TransientModel):
     def _prepare_move_line_domain(self):
         domain = super(AccountPaymentLineCreate, self).\
             _prepare_move_line_domain()
+
+        domain2 = domain.copy()
         if ('date_maturity', '=', False) in domain and self.start_due_date:
-            indx = domain.index(('date_maturity', '=', False)) + 1
-            t = ('date_maturity', '>=', self.start_due_date)
-            domain.insert(indx, t)
+            # Modifico el dominio para que los enncuentre sin fecha
+            # o cuyo vencimiento esté en el ranfo
+            indx = domain.index(('date_maturity', '=', False))
+            t1 = domain.pop(indx)
+            t2 = domain.pop(indx - 1)
+            t3 = ('date_maturity', '>=', self.start_due_date)
+            domain.insert(indx-1, t1)
+            domain.insert(indx, t2)
+            domain.insert(indx+1, t3)
+
+            if self.allow_negative:
+                # Si hay fecha de inicio, el rango no se aplica en abonos
+                # así que vuelvo a añadir la condición para ignorarlos.
+
+                # Ahora en el domain2, hago la misma búysqueda pero sin el
+                # rango, y solo para los negativos
+                if self.order_id.payment_type == 'outbound':
+                    domain.append(('credit', '>', 0))
+                    domain2.append(('credit', '<=', 0))
+                elif self.order_id.payment_type == 'inbound':
+                    domain.append(('debit', '>', 0))
+                    domain2.append(('debit', '<=', 0))
+
+                d1 = expression.normalize_domain(domain)
+                d2 = expression.normalize_domain(domain2)
+                domain = expression.OR([d1, d2])
         return domain
+
+    @api.onchange(
+        'date_type', 'move_date', 'due_date', 'start_due_date',
+        'journal_ids', 'invoice',
+        'target_move', 'allow_blocked', 'payment_mode', 'allow_negative')
+    def move_line_filters_change(self):
+        res = super(AccountPaymentLineCreate, self).move_line_filters_change()
+        return res
