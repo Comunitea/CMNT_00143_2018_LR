d51fb4f5a6cc396f4f2d6543896912b9b2f7bb10
Roodin <santi@comunitea.com>
2019-01-03 19:51:19 +0100

    [ADD] account_custom. Añadimos campos related para el pais y posción fiscal del partener en el apunte

---
+++ project_addons/account_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Account Custom ',
+    'summary': 'Customizations over Account Models',
+    'version': '11.0.1.0.0',
+    'category': 'Custom',
+    'website': 'comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'base',
+        'account'
+    ],
+    'data': [
+
+    ]
+}
---
+++ project_addons/account_custom/models/account_move_line.py
@@ -0,0 +1,15 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, fields
+
+
+class AccountMoveLine(models.Model):
+
+    _inherit = 'account.move.line'
+
+    country_id = fields.Many2one(related='partner_id.country_id', store=True,
+                                 string='Partner Country')
+    account_position_id = fields.Many2one(
+        related='partner_id.property_account_position_id',
+                                    store=True, string='Partner Position')
