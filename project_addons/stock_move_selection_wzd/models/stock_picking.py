73b8b2649f1e634668c3496a9ee7bc1b66159f7d
Kiko Sánchez <kiko@comunitea.com>
2019-04-30 20:07:24 +0200

    [DEV] stock_move_selection_wzd

---
+++ project_addons/stock_move_selection_wzd/models/stock_picking.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2019 Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def create(self, vals):
+        if len(vals) == 1 and vals.get('name', False):
+            domain = [('name', '=', vals.get('name', False))]
+            picking_type_id = self.env['stock.picking.type'].search(domain)
+            if not picking_type_id:
+                raise ValidationError (_("Not pick for '%s'") % vals.get('name', False))
+            if len(picking_type_id)>1:
+                raise ValidationError (_("More than 1 pick for '%s'") % vals.get('name', False))
+            vals.update(picking_type_id=picking_type_id.id,
+                        location_id=picking_type_id.default_location_src_id.id,
+                        location_dest_id=picking_type_id.default_location_dest_id.id)
+            vals.pop('name')
+        return super().create(vals)
---
+++ project_addons/stock_move_selection_wzd/models/warehouse_monitor_wzd.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# © 2019 Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+from datetime import timedelta
+
+class WarehouseMonitorWzd(models.Model):
+
+    _name = "warehouse.monitor.report"
+    _description = 'Warehouse monitor'
+    _inherits = {'stock.picking.type': 'type_id'}
+
+
+
+    @api.multi
+    def get_report_field(self):
+        pick_state = ['waiting', 'confirmed', 'assigned']
+        move_state = ['waiting', 'confirmed', 'assigned', 'partially_available']
+        for type in self:
+
+            moves_domain = [('picking_type_id', '=', type.id), ('picking_id', '=', False), ('state', 'in', move_state)]
+            picking_domain = [('picking_type_id', '=', type.id), ('state', 'in', pick_state)]
+
+            todo_picking_ids = self.env['stock.picking'].search(picking_domain)
+            c_todo_picking_ids = len(todo_picking_ids)
+
+            move_line = self.env['stock.picking'].search(moves_domain)
+            todo_move_line = todo_picking_ids.mapped('move_line')
+
+
+                            # '|', '&', ('state', 'not in', ['draft', 'cancel']), ('date_expected', '<=', fields.datetime.now() + timedelta(days=1)),
+                            # '&', ('state', '=' , 'done'),  ('date', '>=', fields.datetime.now() - timedelta(days=1))
+                            # ]
+
+
+            todo_move_line = move_line.filtered(lambda x:x.state != 'done')
+
+
+
+
+    type_id = fields.Many2one('stock.picking.type')
+
+
+
+    move_line = fields.One2many('stock.move')
+    move_line_ids = fields.One2many('stock.move.line')
+    #picking_ids = fields.One2many('stock.picking')
+    todo_picking_ids = fields.One2many('stock.picking')
+    c_todo_picking_ids = fields.Integer('Pickings to do')
+    todo_move_line = fields.One2many('stock.move')
+    c_todo_move_line = fields.Integer('Moves to do')
+    todo_move_line_ids = fields.One2many('stock.move.line')
+    c_todo_move_line_ids = fields.Integer('Moves to do')
