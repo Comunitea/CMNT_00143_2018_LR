220aaade13e308d57d46db722dbd1c031a7fa2e0
Kiko Sánchez <kiko@comunitea.com>
2019-05-07 16:46:09 +0200

    [FIX] stock_move_apk Añadido controller

---
+++ project_addons/stock_move_apk/controllers/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from openerp import http
+
+_logger = logging.getLogger(__name__)
+
+
+class ProductionAppController(http.Controller):
+
+    @http.route(['/packageApp/'], type='http', auth='public')
+    def a(self, debug=False, **k):
+        return http.local_redirect(
+            '/stock_move_apk/static/www/index.html')
