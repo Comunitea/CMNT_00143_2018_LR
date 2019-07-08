94b672ed39f9817b76f04202ef2af4b3a6a00047
Vicentecom <vicente@comunitea.com>
2019-07-08 11:47:53 +0200

    [ADD] Menu

---
+++ project_addons/theme_rias_menu/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2019 Comunitea - 
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Theme Rías Menu',
+    'version': '11.0.0.0.0',
+    'category': 'Custom',
+    'license': 'AGPL-3',
+    'author': "Comunitea, ",
+    'depends': [
+        'theme_rias'
+    ],
+    'data': [
+        'data/menu_data.xml',
+        'views/website_menu_view.xml',
+        'templates/snippets.xml',
+        'templates/header.xml',
+        'templates/head.xml'
+        ],
+    'images': [
+        '/static/description/icon.png',
+    ],
+    'installable': True,
+}
