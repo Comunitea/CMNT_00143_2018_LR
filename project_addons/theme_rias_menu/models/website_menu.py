0b444da73e56779a599de7e9146c701b90940993
Vicentecom <vicente@comunitea.com>
2019-07-19 15:08:36 +0200

    [FIX] theme_rias_menu: Añadido campo active y arreglado enlace a de los menús

--- project_addons/theme_rias_menu/models/website_menu.py
+++ project_addons/theme_rias_menu/models/website_menu.py
@@ -8,3 +8,4 @@ class Menu(models.Model):
     _inherit = "website.menu"
 
     menu_icon = fields.Char('Icon', default='', help='You can check the full list of icons at https://fontawesome.com/cheatsheet?from=io')
+    active = fields.Boolean('Active', default=False, help="Defines if the menu should be active or not.")
