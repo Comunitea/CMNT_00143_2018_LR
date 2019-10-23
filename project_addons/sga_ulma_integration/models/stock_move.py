ceb0286d0cb722b4ef00aa4c0391fdcba57cac62
Vicentecom <vicente@comunitea.com>
2019-10-23 16:28:56 +0200

    [FIX] sga_ulma_integration: mmmexpordref no se aplicaba a todas las líneas de los pedidos

--- project_addons/sga_ulma_integration/models/stock_move.py
+++ project_addons/sga_ulma_integration/models/stock_move.py
@@ -10,22 +10,21 @@ class StockMoveLine(models.Model):
 
     _inherit = "stock.move.line"
 
-    def get_move_line_ulma_vals(self, cont=0, sale_id=False):
+    def get_move_line_ulma_vals(self, cont=0, picking_id=False):
 
-        sale_id = sale_id or self.move_id.sale_id
-        if sale_id.shipping_type == 'pasaran':
+        if self.picking_id.shipping_type == 'pasaran':
             cte1 = 'P'
-        elif sale_id.shipping_type == 'urgent':
-            cte1 = 'H'
-        else:
+        elif self.picking_id.shipping_type == 'route':
             cte1 = 'N'
+        else:
+            cte1 = 'H'
         vals = self.move_id.picking_type_id.get_ulma_vals('move')
 
         update_vals = {
             'mmmartdes':  self.product_id.display_name[:40],
             'mmmartref': self.product_id.default_code,
             'mmmcanuni': self.product_uom_qty,
-            'mmmexpordref': '{}{}'.format(cte1, sale_id.name)[9:], ##pick.name
+            'mmmexpordref': '{}{}'.format(cte1, self.picking_id.name),
             'mmmsecada': self.id,
             'momcre': datetime.datetime.now(),
             'mmmacccod': cont,
